ff720cbba14d7fb7cdea9aea45e28579392d673f
Ashik <44669509+mashik424@users.noreply.github.com>
2020-03-27 17:29:19 +0530

    Create DistanceLatLng.java

---
+++ DistanceLatLng.java
@@ -0,0 +1,38 @@
+public class DistanceLatLngs {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+
+        System.out.println(distance(11.336777, 75.928332, 11.336866, 75.928040, 'I'));
+    }
+
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515; //miles
+        if (unit == 'K') {
+            dist = dist * 1.609344; //Kilometers
+        } else if (unit == 'N') {
+            dist = dist * 0.8684; // Nautical Miles
+        } else if (unit == 'I') {
+            dist = dist * 1.609344 * 1000; //Meters
+        }
+        return (dist);
+    }
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+}
